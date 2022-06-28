211dc09c6c529a9fd880d2010e54e2af68167eac
krugerrgabriel <gabriel.sa.kruger@gmail.com>
2022-06-28 09:19:21 -0300

    Google Analytics Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,8 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import * as ga from '../lib/ga';
+
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '../styles/global';
@@ -7,6 +11,20 @@ import { defaultTheme } from '../styles/themes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      ga.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Component {...pageProps} />
